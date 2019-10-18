685a678d41b2a27144fa3f57095c305907d76450
James Downs <jamesdownslab@gmail.com>
2019-10-18 16:29:12 +0100

    Split empty runCaesarCipher into encrypt and decrypt functions

--- MPAGS-CIPHER/CaesarCipher.cpp
+++ MPAGS-CIPHER/CaesarCipher.cpp
@@ -1,12 +1,26 @@
 #include "CaesarCipher.hpp"
 
+std::string encryptCaesarCipher(
+    const std::string& inputText
+)
+{
+    return inputText;
+}
+
+std::string decryptCaesarCipher(
+    const std::string& inputText
+)
+{
+    return inputText;
+}
+
 std::string runCaesarCipher(
     const std::string& inputText,
     const bool encrypt){
     if (encrypt){
-        return inputText;
+        return encryptCaesarCipher(inputText);
     }
     else{
-        return inputText;
+        return decryptCaesarCipher(inputText);
     }
 }
