3e146438cb78469cced5ca2e52767904d57b2691
James Downs <jamesdownslab@gmail.com>
2019-10-18 14:33:34 +0100

    Updated naming for consistency

--- MPAGS-CIPHER/TransformChar.hpp
+++ MPAGS-CIPHER/TransformChar.hpp
@@ -4,6 +4,6 @@
 #include <string>
 
 //! Transliterate char to string
-std::string transformChar(const char in);
+std::string transformChar(const char in_char);
 
 #endif // MPAGSCIPHER_TRANSFORMCHAR_HPP
