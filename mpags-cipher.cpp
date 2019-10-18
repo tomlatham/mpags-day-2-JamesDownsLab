617b1ee0371e06a39a8557a0b916e8a8d03d6c9d
James Downs <jamesdownslab@gmail.com>
2019-10-18 13:46:24 +0100

    Moved functions to end of file

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,60 +6,7 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
-std::string transformChar(const char in_char){
-  /* Transform the input characters to the required input.
-  
-  All letters are returned uppercase.
-  All digits are returned as the uppercase spelling of their number.
-  All other symbols are ignored.
-  */
-
-  // Uppercase alphabetic characters
-  std::string out_string {""};
-
-  if (std::isalpha(in_char)) {
-    out_string = std::toupper(in_char);
-  }
-  else {
-    // Transliterate digits to English words
-    switch (in_char) {
-      case '0':
-        out_string = "ZERO";
-        break;
-      case '1':
-        out_string = "ONE";
-        break;
-      case '2':
-        out_string = "TWO";
-        break;
-      case '3':
-        out_string = "THREE";
-        break;
-      case '4':
-        out_string = "FOUR";
-        break;
-      case '5':
-        out_string = "FIVE";
-        break;
-      case '6':
-        out_string = "SIX";
-        break;
-      case '7':
-        out_string = "SEVEN";
-        break;
-      case '8':
-        out_string = "EIGHT";
-        break;
-      case '9': 
-        out_string = "NINE";
-        break;
-      default:
-        out_string = "";
-        break;
-    }
-  }
-  return out_string;
-}
+std::string transformChar(const char in_char);
 
 bool processCommandLine (
   const std::vector<std::string>& cmdLineArgs,
@@ -67,54 +14,7 @@ bool processCommandLine (
   bool& versionRequested,
   std::string& inputFile,
   std::string& outputFile
-)
-{
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
-
-  for (size_type i {1}; i < nCmdLineArgs; i++) {
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (cmdLineArgs[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (cmdLineArgs[i] == "-i") {
-      // Handle input file option
-      // NExt element is filename unless -i is the last argument
-      if (i == nCmdLineArgs -1) {
-        std::cerr << "[error] -i requires a filename argument" << std::endl;
-        // exit with non-zero
-        return 1;
-      }
-      else {
-        // Got filename, so assign value and advance past it
-        inputFile = cmdLineArgs[i+1];
-        ++i;
-      }
-    }
-    else if (cmdLineArgs[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-        std::cerr << "[error] -o requires a filename argument" << std::endl;
-        // exit with non-zero
-        return 1;
-      }
-      else {
-        outputFile = cmdLineArgs[i+1];
-        ++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
-      // Exit with non-zero
-      return 1;
-    }
-  }
-  return 0;
-}
+);
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
@@ -200,4 +100,114 @@ int main(int argc, char* argv[])
   // No requirement to return from main, but we do so for clarity
   // and for consistency with other functions
   return 0;
+}
+
+bool processCommandLine (
+  const std::vector<std::string>& cmdLineArgs,
+  bool& helpRequested,
+  bool& versionRequested,
+  std::string& inputFile,
+  std::string& outputFile
+)
+{
+  typedef std::vector<std::string>::size_type size_type;
+  const size_type nCmdLineArgs {cmdLineArgs.size()};
+
+  for (size_type i {1}; i < nCmdLineArgs; i++) {
+    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
+      helpRequested = true;
+    }
+    else if (cmdLineArgs[i] == "--version") {
+      versionRequested = true;
+    }
+    else if (cmdLineArgs[i] == "-i") {
+      // Handle input file option
+      // NExt element is filename unless -i is the last argument
+      if (i == nCmdLineArgs -1) {
+        std::cerr << "[error] -i requires a filename argument" << std::endl;
+        // exit with non-zero
+        return 1;
+      }
+      else {
+        // Got filename, so assign value and advance past it
+        inputFile = cmdLineArgs[i+1];
+        ++i;
+      }
+    }
+    else if (cmdLineArgs[i] == "-o") {
+      // Handle output file option
+      // Next element is filename unless -o is the last argument
+      if (i == nCmdLineArgs-1) {
+        std::cerr << "[error] -o requires a filename argument" << std::endl;
+        // exit with non-zero
+        return 1;
+      }
+      else {
+        outputFile = cmdLineArgs[i+1];
+        ++i;
+      }
+    }
+    else {
+      // Have an unknown flag to output error message
+      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
+      // Exit with non-zero
+      return 1;
+    }
+  }
+  return 0;
+}
+
+std::string transformChar(const char in_char){
+  /* Transform the input characters to the required input.
+  
+  All letters are returned uppercase.
+  All digits are returned as the uppercase spelling of their number.
+  All other symbols are ignored.
+  */
+
+  // Uppercase alphabetic characters
+  std::string out_string {""};
+
+  if (std::isalpha(in_char)) {
+    out_string = std::toupper(in_char);
+  }
+  else {
+    // Transliterate digits to English words
+    switch (in_char) {
+      case '0':
+        out_string = "ZERO";
+        break;
+      case '1':
+        out_string = "ONE";
+        break;
+      case '2':
+        out_string = "TWO";
+        break;
+      case '3':
+        out_string = "THREE";
+        break;
+      case '4':
+        out_string = "FOUR";
+        break;
+      case '5':
+        out_string = "FIVE";
+        break;
+      case '6':
+        out_string = "SIX";
+        break;
+      case '7':
+        out_string = "SEVEN";
+        break;
+      case '8':
+        out_string = "EIGHT";
+        break;
+      case '9': 
+        out_string = "NINE";
+        break;
+      default:
+        out_string = "";
+        break;
+    }
+  }
+  return out_string;
 }
