6d7d41e300332728718bcaac7141d2408fca45ae
James Downs <jamesdownslab@gmail.com>
2019-10-18 15:47:18 +0100

    Moved readInput function to its own file

---
+++ MPAGS-CIPHER/readInput.cpp
@@ -0,0 +1,30 @@
+#include "readInput.hpp"
+#include "TransformChar.hpp"
+
+std::string read_input(std::string inputFile){
+  char inputChar {'x'};
+  std::string inputText{""};
+
+    if (!inputFile.empty()) {
+    std::ifstream in_file {inputFile};
+    bool ok_to_read {in_file.good()};
+    if (ok_to_read){
+      while (in_file >> inputChar){
+        // Loop over each character from the input file
+        inputText += transformChar(inputChar);
+      }
+    }
+    else{
+      std::cerr << "Error reading File" << std::endl;
+    }
+  }
+  else{
+    // Loop over each character from user input
+    // (until Return then CTRL-D (EOF) pressed)
+    while(std::cin >> inputChar)
+    {
+      inputText += transformChar(inputChar);
+    }
+  }
+  return inputText;
+}
---
+++ MPAGS-CIPHER/readInput.hpp
@@ -0,0 +1,10 @@
+#ifndef MPAGSCIPHER_READ_INPUT_HPP
+#define MPAGSCIPHER_READ_INPUT_HPP
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+std::string read_input(std::string inputFile);
+
+#endif
