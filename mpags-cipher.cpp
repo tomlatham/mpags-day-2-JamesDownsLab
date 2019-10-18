75d51098631327357af3af0c181f593f87b8ce53
James Downs <jamesdownslab@gmail.com>
2019-10-18 11:07:07 +0100

    changed transformChar to only have one return

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -15,39 +15,50 @@ std::string transformChar(const char in_char){
   */
 
   // Uppercase alphabetic characters
-  
-  if (std::isalpha(in_char)) {
-    std::string out_string{""};
-    out_string += std::toupper(in_char);
-    return out_string;
-    // return std::to_string(std::toupper(in_char));
-  }
+  std::string out_string {""};
 
-  // Transliterate digits to English words
-  switch (in_char) {
-    case '0':
-      return "ZERO";
-    case '1':
-      return "ONE";
-    case '2':
-      return "TWO";
-    case '3':
-      return "THREE";
-    case '4':
-      return "FOUR";
-    case '5':
-      return "FIVE";
-    case '6':
-      return "SIX";
-    case '7':
-      return "SEVEN";
-    case '8':
-      return "EIGHT";
-    case '9': 
-      return "NINE";
-    default:
-      return "";
+  if (std::isalpha(in_char)) {
+    out_string = std::toupper(in_char);
   }
+  else {
+    // Transliterate digits to English words
+    switch (in_char) {
+      case '0':
+        out_string = "ZERO";
+        break;
+      case '1':
+        out_string = "ONE";
+        break;
+      case '2':
+        out_string = "TWO";
+        break;
+      case '3':
+        out_string = "THREE";
+        break;
+      case '4':
+        out_string = "FOUR";
+        break;
+      case '5':
+        out_string = "FIVE";
+        break;
+      case '6':
+        out_string = "SIX";
+        break;
+      case '7':
+        out_string = "SEVEN";
+        break;
+      case '8':
+        out_string = "EIGHT";
+        break;
+      case '9': 
+        out_string = "NINE";
+        break;
+      default:
+        out_string = "";
+        break;
+    }
+  }
+  return out_string;
 }
 
 // Main function of the mpags-cipher program
