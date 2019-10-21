863808ff84654028398df3b7dcb4d7bd330ab25f
James Downs <jamesdownslab@gmail.com>
2019-10-21 19:40:21 +0100

    Added comments and reformat

--- MPAGS-CIPHER/writeOutput.cpp
+++ MPAGS-CIPHER/writeOutput.cpp
@@ -1,20 +1,21 @@
 #include "writeOutput.hpp"
 
 void write_output(
-  const std::string& outputFile,
-  const std::string& outputText)
-  {
+        const std::string &outputFile,
+        const std::string &outputText) {
+    /*
+     * Writes outputText to outputFile. If outputFile is an empty string
+     * write the output to the console.
+     */
     if (!outputFile.empty()) {
-      std::ofstream out_file {outputFile};
-      bool ok_to_write {out_file.good()};
-    if (ok_to_write){
-      out_file << outputText;
+        std::ofstream out_file{outputFile};
+        bool ok_to_write{out_file.good()};
+        if (ok_to_write) {
+            out_file << outputText;
+        } else {
+            std::cerr << "Error writing file" << std::endl;
+        }
+    } else {
+        std::cout << outputText << std::endl;
     }
-    else{
-        std::cerr << "Error writing file" << std::endl;
-    }
-  }
-  else{
-    std::cout << outputText << std::endl;
-  }
 }
