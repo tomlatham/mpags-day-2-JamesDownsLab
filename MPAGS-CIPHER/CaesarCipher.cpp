435b49bd6b89d41ca2b4896ba9df90efecd69cbf
James Downs <jamesdownslab@gmail.com>
2019-10-21 14:23:56 +0100

    Moved Caesar Cipher to one function to avoid repetition

--- MPAGS-CIPHER/CaesarCipher.cpp
+++ MPAGS-CIPHER/CaesarCipher.cpp
@@ -1,51 +1,25 @@
 #include "CaesarCipher.hpp"
 
-std::string encryptCaesarCipher(
-    const std::string& inputText,
-    const int& key){
+std::string runCaesarCipher(
+        const std::string&inputText,
+        const bool& encrypt,
+        const int& key){
     std::string outputText;
     const std::string alphabet{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    int position{0};
+    int pos{0};
     for (char i : inputText){
-        position = 0;
-        for (char j : alphabet){
-            if (i == j){
-                outputText += alphabet[(position + key)%alphabet.size()];
-                break;
-            }
-            ++position;
-        }
-    }
-    return outputText;
-}
-
-std::string decryptCaesarCipher(
-    const std::string& inputText,
-    const int& key){
-    std::string outputText;
-    const std::string alphabet{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    int position{0};
-    for (char i : inputText){
-        position = 0;
+        pos = 0;
         for (char j : alphabet){
             if (i==j){
-                outputText += alphabet[(position - key)%alphabet.size()];
+                if (encrypt){
+                    outputText += alphabet[(pos+key)%alphabet.size()];
+                } else{
+                    outputText += alphabet[(pos - key)%alphabet.size()];
+                }
                 break;
             }
-            ++position;
+            ++pos;
         }
     }
     return outputText;
-}
-
-std::string runCaesarCipher(
-    const std::string& inputText,
-    const bool& encrypt,
-    const int& key){
-    if (encrypt){
-        return encryptCaesarCipher(inputText, key);
-    }
-    else{
-        return decryptCaesarCipher(inputText, key);
-    }
 }
