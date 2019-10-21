48216a3d306168e4d47ef45761098b0717fbe1b7
James Downs <jamesdownslab@gmail.com>
2019-10-21 14:41:28 +0100

    Moved help to its own function

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -9,6 +9,7 @@
 #include "readInput.hpp"
 #include "writeOutput.hpp"
 #include "CaesarCipher.hpp"
+#include "PrintHelp.h"
 
 
 // Main function of the mpags-cipher program
@@ -42,19 +43,7 @@ int main(int argc, char *argv[]) {
 
     // Handle help, if requested
     if (helpRequested) {
-        // Line splitting for readability
-        std::cout
-                << "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
-                << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-                << "Available options:\n\n"
-                << "  -h|--help        Print this help message and exit\n\n"
-                << "  --version        Print version information\n\n"
-                << "  -i FILE          Read text to be processed from FILE\n"
-                << "                   Stdin will be used if not supplied\n\n"
-                << "  -o FILE          Write processed text to FILE\n"
-                << "                   Stdout will be used if not supplied\n\n";
-        // Help requires no further action, so return from main
-        // with 0 used to indicate success
+        printHelp();
         return 0;
     }
 
