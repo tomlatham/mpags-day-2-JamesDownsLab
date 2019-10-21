d7d13b3b4e8bb540185d75554f473093290786f9
James Downs <jamesdownslab@gmail.com>
2019-10-21 12:37:34 +0100

    Implemented Caesar encryption

--- MPAGS-CIPHER/CaesarCipher.cpp
+++ MPAGS-CIPHER/CaesarCipher.cpp
@@ -1,10 +1,13 @@
 #include "CaesarCipher.hpp"
 
 std::string encryptCaesarCipher(
-    const std::string& inputText
-)
-{
-    return inputText;
+    const std::string& inputText,
+    const int& key){
+    std::string outputText;
+    for (std::string::size_type i = 0; i < inputText.size(); ++i){
+        outputText += inputText[(i + key)%inputText.size()];
+    }
+    return outputText;
 }
 
 std::string decryptCaesarCipher(
@@ -18,9 +21,8 @@ std::string runCaesarCipher(
     const std::string& inputText,
     const bool& encrypt,
     const int& key){
-    (void)key;
     if (encrypt){
-        return encryptCaesarCipher(inputText);
+        return encryptCaesarCipher(inputText, key);
     }
     else{
         return decryptCaesarCipher(inputText);
